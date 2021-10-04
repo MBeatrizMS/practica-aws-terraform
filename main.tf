413dfef7b7c2bed1cc3fe15ddf7cf9f2a8a8da0e
Belatrix <btrizsamper@gmail.com>
2021-10-03 19:33:19 -0600

    Cambio en main de terraform por zona no disponible

--- main.tf
+++ main.tf
@@ -1,13 +1,21 @@
 provider "aws" {
   version = "~> 2.7"
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "web" {  
-  ami = "ami-077e31c4939f6a2f3"
+  ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+  
+   tags = {
+     Name = "devops-2-YenKey"
+   }
 }
 
 output "public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
+
+ output "private_ip" {
+   value = "${aws_instance.web.private_ip}"
+ }
